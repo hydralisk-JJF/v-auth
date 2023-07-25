a7aedabff03acd6fa7b0f05fed77572ac22c654c
jiangjfa <jiangjfa@enn.cn>
2023-07-25 15:23:52 +0800

    upd: delete log

--- src/authConstruct.js
+++ src/authConstruct.js
@@ -1,34 +1,25 @@
-import lodash from "lodash";
-const { isArray, intersection } = lodash;
 import { watchEffect } from "vue";
 
 export default class AuthControl {
   constructor(options) {
-    console.log("===自定义指令注入配置1222");
     this.authCodeList = options.authCodeList;
   }
   isPermission = (value) => {
     if (!value) {
       return true;
     }
-    if (!isArray(value)) {
-      return this.authCodeList.includes(value);
-    }
-    return intersection(value, this.authCodeList).length > 0;
+    return this.authCodeList.includes(value);
   };
   removeElement = (el) => {
     el._parentNode = el.parentNode;
     el._placeholderNode = document.createComment("auth");
     el.parentNode?.replaceChild(el._placeholderNode, el);
-    console.log("===移除节点");
   };
   addElement = (el) => {
     el._parentNode?.replaceChild(el, el._placeholderNode);
-    console.log("===增加节点");
   };
   authInMounted = (el, binding) => {
     const value = binding.value;
-    console.log("===指令初始化成功");
     if (!value) return;
     if (!this.isPermission(value)) {
       this.removeElement(el);
@@ -36,7 +27,6 @@ export default class AuthControl {
   };
   authInUpdated = (el, binding) => {
     const dataUpdated = () => {
-      console.log(binding.value, binding.oldValue, "===调用dataUpdated");
       if (binding.value === binding.oldValue) return;
       const permissionBeforeUpdate = this.isPermission(binding.oldValue);
       const permissionAfterUpdate = this.isPermission(binding.value);
@@ -49,12 +39,10 @@ export default class AuthControl {
     };
     if (el._watchEffect) {
       dataUpdated();
-      console.log("===bind值变化");
     } else {
       el._watchEffect = watchEffect(() => {
         dataUpdated();
       });
-      console.log("===挂载一个watchEffect");
     }
   };
 }
